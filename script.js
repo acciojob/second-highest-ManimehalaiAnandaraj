0eb36368c318a78bb41aab1d34b03eee3527a392
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-16 20:14:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function seconghigh(arr)
+{
+If(arr===0 || arr.length<=1)
+{
+return;
+}
+Let uniarr=[…new set(arr)];
+Uniarr.sort((arr[i],arr[i+1]) => arr[i+1]-arr[i]);
+Return uniarr[1];
+}
+
+
+	
